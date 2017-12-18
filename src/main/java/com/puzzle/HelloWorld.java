2d4a881279cb2714be3f251209001ea43de6658f
SongCobweb <swq11823386@163.com>
2017-12-18 18:04:58 +0800

    slf4j+log4j2

--- src/main/java/com/puzzle/HelloWorld.java
+++ src/main/java/com/puzzle/HelloWorld.java
@@ -1,8 +1,18 @@
 package com.puzzle;
 
-public class HelloWorld {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-	public static void main(String[] args) {
-		System.out.println("HelloWorld");
+public class HelloWorld {
+	protected static Logger logger = LoggerFactory.getLogger(HelloWorld.class);
+
+	public static void main(String[] args) throws InterruptedException {
+		Thread.sleep(1000L);
+		for(int i = 0; i <= 10000; i++){
+			logger.info("before");
+			System.out.println("HelloWorld");
+			logger.info("after");
+			logger.error("error");
+		}
 	}
 }
