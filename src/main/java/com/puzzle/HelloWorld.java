c7e2809937f56c3dcc1b790816f75ab7ef5264fc
SongCobweb <swq11823386@163.com>
2017-12-21 22:05:52 +0800

    daily push

--- src/main/java/com/puzzle/HelloWorld.java
+++ src/main/java/com/puzzle/HelloWorld.java
@@ -1,19 +1,22 @@
 package com.puzzle;
 
+import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import redis.clients.jedis.Jedis;
 
 public class HelloWorld {
 	protected static Logger logger = LoggerFactory.getLogger(HelloWorld.class);
 
 	public static void main(String[] args) throws InterruptedException {
 		
-		Thread.sleep(1000L);
-		for(int i = 0; i <= 10000; i++){
-			logger.info("before");
-			System.out.println("HelloWorld");
-			logger.info("after");
-			logger.error("error");
-		}
+		
+	}
+	
+	@Test
+	public void test01() {
+		Jedis jedis = new Jedis("127.0.0.1", 6379);
+		System.out.println(jedis.ping());
 	}
 }
